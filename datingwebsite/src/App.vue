0c0314c6b076b4894cb104e9483c0355d1486f27
oraclelens <sophiar10200@gmail.com>
2023-05-10 13:15:11 -0400

    colors

---
+++ datingwebsite/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1><u>Dating App</u></h1>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+h1 {
+  margin: auto;
+  text-align: center;
+  margin-bottom: 20px;
+  font-weight: bold;
+  color: rgb(255, 88, 88);
+  font-size: 7vh;
+  font-family: 'Shrikhand', cursive;
+}
+</style>
