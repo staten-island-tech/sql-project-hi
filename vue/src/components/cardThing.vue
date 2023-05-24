e7b45b6b8dafa1aeb0dca95fa05902910d936172
cassidyrobles <cassidyrobles19@gmail.com>
2023-05-24 13:14:43 -0400

    sdfg

---
+++ vue/src/components/cardThing.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="card">
+    <h2>{{name}}</h2>
+    <img :src="image"/>
+    <p>{{ ranking }}</p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: {
+            title: String,
+            image: String,
+            ranking: Number
+        },
+
+    }
+
+</script>
+
+<style scoped>
+
+
+
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 5px solid var(--fourth);
+  box-sizing: border-box;
+  margin: 20px;
+  border-radius: 20px;
+  background-color: var(--secondary);
+  text-align: center;
+  box-shadow: 0 20px 20px 10px rgba(0, 0, 0, 0.5);
+  line-height: 1.1;
+  width: 30%;
+  font-family: 'Courier Prime', monospace;
+}
+
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h1>Hadestown Playlist</h1>
+    <Card
+    />
+    <div class="box">
+      
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
+import Card from "../components/cardThing.vue";
+songs [
+
+]
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
